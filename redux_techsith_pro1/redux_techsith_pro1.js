f8811491be147a500011aaa7f7473a68ee1d6d84
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-05-20 00:10:33 +0530

    adding payload

--- redux_techsith_pro1/redux_techsith_pro1.js
+++ redux_techsith_pro1/redux_techsith_pro1.js
@@ -8,12 +8,14 @@ const myReducer = (state  = initialState, action ) => {
 
  if(action.type ==='ADD') 
  {
-     newState.age += 1; 
+     //newState.age += 1;  
+     newState.age += action.val;
  }  
 
  if(action.type ==='SUBSTRACT') 
  {
-     newState.age -= 1; 
+    // newState.age -= 1;  
+    newState.age -= action.val;
  } 
  return newState; 
 
@@ -29,18 +31,18 @@ const store = createStore(myReducer);
 
 
 
-store.dispatch({ type:'ADD'}); 
+//store.dispatch({ type:'ADD'}); 
 //console.log('after add' +JSON.stringify(store.getState())); 
 
-store.dispatch({type:'SUBSTRACT'});
+//store.dispatch({type:'SUBSTRACT'});
 //console.log( 'after substract' +JSON.stringify(store.getState()));
 
 //keep adding store .dispatch  
-store.dispatch({ type:'ADD'}); 
-store.dispatch({ type:'ADD'}); 
-store.dispatch({ type:'ADD'}); 
-store.dispatch({ type:'ADD'}); 
-store.dispatch({ type:'ADD'}); 
-store.dispatch({ type:'ADD'}); 
-store.dispatch({ type:'ADD'}); 
-store.dispatch({type:'SUBSTRACT'});
+store.dispatch({ type:'ADD', val:10}); 
+//store.dispatch({ type:'ADD'}); 
+// store.dispatch({ type:'ADD'}); 
+// store.dispatch({ type:'ADD'}); 
+// store.dispatch({ type:'ADD'}); 
+// store.dispatch({ type:'ADD'}); 
+// store.dispatch({ type:'ADD'}); 
+store.dispatch({type:'SUBSTRACT', val:5});
