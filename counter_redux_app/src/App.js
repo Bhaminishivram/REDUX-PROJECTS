21e858e68e2dd5f073e8601290de0d57c91e82c6
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-05-26 23:23:04 +0530

    counter app updated

--- counter_redux_app/src/App.js
+++ counter_redux_app/src/App.js
@@ -1,12 +1,26 @@
-
+ import React from 'react';
 import './App.css';
+import  {useSelector,useDispatch} from 'react-redux'; 
+import {increment} from './actions/demo'; 
+import {decrement} from './actions/demo'; 
 
-function App() {
-  return (
+function App() { 
+ const counter = useSelector
+ (state => state.counter); 
+
+ const logged = useSelector
+ (state => state.logged);
+  
+ const dispatch = useDispatch(); 
+   return(
     <div className="App">
-      <h1>Hello counter</h1>
+      <h1>Counter{counter}</h1>    
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+    {logged ? <h3> Valuable information i shoundn't see</h3>:''}
     </div>
-  );
-}
+   );
+};
 
 export default App;
--- counter_redux_app/src/index.js
+++ counter_redux_app/src/index.js
@@ -3,53 +3,64 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'; 
 import {createStore} from 'redux' ;   
+import allReducer from './reducers/demo'; 
+import {Provider} from 'react-redux'; 
 
+const myStore = createStore(
+   allReducer, 
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
+
+   ReactDOM.render( 
+    <Provider store = {myStore}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
  //Store ---> Globalized state 
 
  //Action  increment   
- const increment = () => {
-   return {
-     type: 'INCREMENT'
-   };
- } ;
+//  const increment = () => {
+//    return {
+//      type: 'INCREMENT'
+//    };
+//  } ;
 
- const decrement =() => {
-   return {
-     type: 'DECREMENT' 
-    };
- };
+//  const decrement =() => {
+//    return {
+//      type: 'DECREMENT' 
+//     };
+//  };
 
 
  //Reducer 
-   const counter = (state =0 , action) => 
-   {
-        switch(action.type){
-          case"INCREMENT" :
-                return state + 1; 
-          case "DECREMENT": 
-                return state - 1; 
-        }
-   }
+  //  const counter = (state =0 , action) => 
+  //  {
+  //       switch(action.type){
+  //         case"INCREMENT" :
+  //               return state + 1; 
+  //         case "DECREMENT": 
+  //               return state - 1; 
+  //       }
+  //  }
 
    
-   let store = createStore(counter);  
+  //  let store = createStore(counter);  
    //display it in the console 
-   store.subscribe(() => console.log(store.getState())); 
+  // store.subscribe(() => console.log(store.getState())); 
  
  //Dispatch  
- store.dispatch(increment());
- store.dispatch(increment()); 
- store.dispatch(increment());
- store.dispatch(increment());
- store.dispatch(increment());
-store.dispatch(decrement()); 
-store.dispatch(decrement()); 
-store.dispatch(decrement())
-store.dispatch(decrement())
+//  store.dispatch(increment());
+//  store.dispatch(increment()); 
+//  store.dispatch(increment());
+//  store.dispatch(increment());
+//  store.dispatch(increment());
+// store.dispatch(decrement()); 
+// store.dispatch(decrement()); 
+// store.dispatch(decrement())
+// store.dispatch(decrement())
 
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+
+
+
